339b949a60a4e8965d63a722a01102e713096235
jimxl <tianxiaxl@gmail.com>
2018-11-06 11:07:50 +0800

    更换logger了中间件的位置

--- gream/web/middleware/middleware.go
+++ gream/web/middleware/middleware.go
@@ -1,8 +1,11 @@
 package middleware
 
 import (
+	"fmt"
+	"gbs/gream/logger"
 	"gbs/gream/web/http_router"
 	. "gbs/gream/web/response"
+	"time"
 
 	"net/http"
 
@@ -14,6 +17,7 @@ var re *mux.Router
 func init() {
 	re = http_router.Router()
 	re.Use(responseMiddleware)
+	Use(loggerMiddleWare)
 }
 
 func responseMiddleware(next http.Handler) http.Handler {
@@ -31,4 +35,36 @@ func (f HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func Use(mwf ...mux.MiddlewareFunc) {
 	re.Use(mwf...)
+}
+
+func loggerMiddleWare(next http.Handler) http.Handler {
+	return HandlerFunc(func(response *Response, r *http.Request) {
+		t := time.Now()
+
+		requestMethod := r.Method
+		url := r.RequestURI
+
+		logger.Info(
+			fmt.Sprintf(
+				"Started %s \"%s\" for %s at %s",
+				requestMethod,
+				url,
+				r.RemoteAddr,
+				t.Format("2006-01-02 15:04:05 +0800"),
+			))
+
+		next.ServeHTTP(response, r)
+
+		latency := time.Since(t)
+		status := response.StatusCode()
+
+		logger.Info(
+			fmt.Sprintf(
+				"Completed %s %v in %v",
+				http.StatusText(status),
+				status,
+				latency,
+			))
+
+	})
 }
