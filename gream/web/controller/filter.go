51f11e3462e8341a25ff1241dc2eaaafd47a6a8d
jimxl <tianxiaxl@gmail.com>
2018-11-18 00:24:06 +0800

    去掉了debug

--- gream/web/controller/filter.go
+++ gream/web/controller/filter.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"errors"
-	"fmt"
 	"gbs/gream/logger"
 	"reflect"
 )
@@ -31,12 +30,12 @@ func (controller *BaseController) CallActionWithFilter(actionName string, self r
 
 func (controller *BaseController) callAction(actionName string, self reflect.Value) {
 	action := self.MethodByName(actionName)
-	fmt.Printf("3 %+v\n", self.Elem().Type())
-	fmt.Printf("4 %+v\n", reflect.ValueOf(controller).Elem().Type())
-	fmt.Printf("5 %+v\n", controller)
-
-	fmt.Printf("6 %+v\n", self.Pointer())
-	fmt.Printf("7 %+v\n", reflect.ValueOf(controller).Pointer())
+	// TODO: 为啥golang当中指针一样，却不能反射呢
+	//fmt.Printf("3 %+v\n", self.Elem().Type())
+	//fmt.Printf("4 %+v\n", reflect.ValueOf(controller).Elem().Type())
+	//fmt.Printf("5 %+v\n", controller)
+	//fmt.Printf("6 %+v\n", self.Pointer())
+	//fmt.Printf("7 %+v\n", reflect.ValueOf(controller).Pointer())
 	if !action.IsValid() {
 		err := errors.New("action invalid1")
 		logger.Error(err.Error())
