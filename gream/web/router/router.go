8f34ad5c1dca79bb2c91d1588cc588bde2d8edd8
jimxl <tianxiaxl@gmail.com>
2018-11-18 17:50:23 +0800

    修改了controller的写法

--- gream/web/router/router.go
+++ gream/web/router/router.go
@@ -14,12 +14,12 @@ var ApplicationRouterDraw func()
 var r = &Router{}
 
 var (
-	HEAD      = r.HEAD
-	GET       = r.GET
-	POST      = r.POST
-	PUT       = r.PUT
-	PATCH     = r.PATCH
-	DELETE    = r.DELETE
+	HEAD   = r.HEAD
+	GET    = r.GET
+	POST   = r.POST
+	PUT    = r.PUT
+	PATCH  = r.PATCH
+	DELETE = r.DELETE
 
 	Resources = r.Resources
 	Namespace = r.Namespace
@@ -30,84 +30,81 @@ func urlJoin(paths ...string) string {
 	return filepath.Join(paths...)
 }
 
+func withOpt(to string, opts []H, f func(opt H)) {
+	if len(opts) <= 0 {
+		f(H{"to": to})
+	} else {
+		opt := H{"to": to}
+		rmap.Merge(&opt, opts[0])
+		f(opt)
+	}
+}
+
 type Router struct {
 	urlSpace    string
 	moduleSpace string
 }
 
-func (r *Router) GET(path string, opt H) {
-	route := r.getRoute(http.MethodGet, path, opt)
-	http_router.GET(route.fullpath, route.getHandle())
+func (r *Router) GET(path, to string, opts ...H) {
+	withOpt(to, opts, func(opt H) {
+		route := r.getRoute(http.MethodGet, path, opt)
+		http_router.GET(route.fullpath, route.getHandle())
+	})
 }
 
-func (r *Router) POST(path string, opt H) {
-	route := r.getRoute(http.MethodPost, path, opt)
-	http_router.POST(route.fullpath, route.getHandle())
+func (r *Router) POST(path, to string, opts ...H) {
+	withOpt(to, opts, func(opt H) {
+		route := r.getRoute(http.MethodPost, path, opt)
+		http_router.POST(route.fullpath, route.getHandle())
+	})
 }
 
-func (r *Router) HEAD(path string, opt H) {
-	route := r.getRoute(http.MethodPost, path, opt)
-	http_router.HEAD(route.fullpath, route.getHandle())
+func (r *Router) HEAD(path, to string, opts ...H) {
+	withOpt(to, opts, func(opt H) {
+		route := r.getRoute(http.MethodPost, path, opt)
+		http_router.HEAD(route.fullpath, route.getHandle())
+	})
 }
 
-func (r *Router) PUT(path string, opt H) {
-	route := r.getRoute(http.MethodPost, path, opt)
-	http_router.PUT(route.fullpath, route.getHandle())
+func (r *Router) PUT(path, to string, opts ...H) {
+	withOpt(to, opts, func(opt H) {
+		route := r.getRoute(http.MethodPost, path, opt)
+		http_router.PUT(route.fullpath, route.getHandle())
+	})
 }
 
-func (r *Router) PATCH(path string, opt H) {
-	route := r.getRoute(http.MethodPost, path, opt)
-	http_router.PATCH(route.fullpath, route.getHandle())
+func (r *Router) PATCH(path, to string, opts ...H) {
+	withOpt(to, opts, func(opt H) {
+		route := r.getRoute(http.MethodPost, path, opt)
+		http_router.PATCH(route.fullpath, route.getHandle())
+	})
 }
 
-func (r *Router) DELETE(path string, opt H) {
-	route := r.getRoute(http.MethodDelete, path, opt)
-	http_router.DELETE(route.fullpath, route.getHandle())
+func (r *Router) DELETE(path, to string, opts ...H) {
+	withOpt(to, opts, func(opt H) {
+		route := r.getRoute(http.MethodDelete, path, opt)
+		http_router.DELETE(route.fullpath, route.getHandle())
+	})
 }
 
-func (r *Router) Any(path string, opt H) {
-	route := r.getRoute(http.MethodDelete, path, opt)
-	http_router.Any(route.fullpath, route.getHandle())
+func (r *Router) Any(path, to string, opts ...H) {
+	withOpt(to, opts, func(opt H) {
+		route := r.getRoute(http.MethodDelete, path, opt)
+		http_router.Any(route.fullpath, route.getHandle())
+	})
 }
 
 func (r *Router) Resources(name string, opts ...H) {
-	pluralName, singularName := rstring.Plural(name), rstring.Singular(name)
+	pluralName := rstring.Plural(name)
 
-	var opt H
-	if len(opts) <= 0 {
-		opt = H{}
-	} else {
-		opt = opts[0]
-	}
-
-	indexOpt := H{"to": pluralName + "#index"}
-	rmap.Merge(&indexOpt, opt)
-	r.GET(urlJoin("/", pluralName), indexOpt)
-
-	newOpt := H{"to": pluralName + "#new"}
-	rmap.Merge(&newOpt, opt)
-	r.GET(urlJoin("/", singularName, "new"), newOpt)
-
-	createOpt := H{"to": pluralName + "#create"}
-	rmap.Merge(&newOpt, opt)
-	r.POST(urlJoin("/", pluralName), createOpt)
-
-	showOpt := H{"to": pluralName + "#show"}
-	rmap.Merge(&showOpt, opt)
-	r.GET(urlJoin("/", pluralName, ":id"), showOpt)
-
-	editOpt := H{"to": pluralName + "#edit"}
-	rmap.Merge(&editOpt, opt)
-	r.GET(urlJoin("/", pluralName, ":id/edit"), editOpt)
-
-	updateOpt := H{"to": pluralName + "#update"}
-	rmap.Merge(&updateOpt, opt)
-	r.PATCH(urlJoin("/", pluralName, ":id"), updateOpt)
-	r.PUT(urlJoin("/", pluralName, ":id"), updateOpt)
-
-	deleteOpt := H{"to": pluralName + "#destroy"}
-	rmap.Merge(&updateOpt, opt)
-	r.DELETE(urlJoin("/", pluralName, ":id"), deleteOpt)
+	r.GET(urlJoin("/", pluralName), pluralName+"#index", opts...)
+	r.GET(urlJoin("/", pluralName, "new"), pluralName+"#new", opts...)
+	r.POST(urlJoin("/", pluralName), pluralName+"#create", opts...)
+	r.GET(urlJoin("/", pluralName, "{id}"), pluralName+"#show", opts...)
+	r.GET(urlJoin("/", pluralName, "{id}/edit"), pluralName+"#edit", opts...)
+	r.PUT(urlJoin("/", pluralName, "{id}"), pluralName+"#update", opts...)
+	r.PATCH(urlJoin("/", pluralName, "{id}"), pluralName+"#update", opts...)
+	r.DELETE(urlJoin("/", pluralName, "{id}"), pluralName+"#destroy", opts...)
 }
 
 func (r *Router) Namespace(space string) *Router {
